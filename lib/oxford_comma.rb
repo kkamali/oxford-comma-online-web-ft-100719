d779df2add7afadb34a90ae8ab620fa556848e19
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 16:53:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     new_word = ""
     array.each_with_index do |part, index|
       if index + 1 == array.length
-        puts "Hey??"
         new_word << ", and "
       else
         new_word << ", "
