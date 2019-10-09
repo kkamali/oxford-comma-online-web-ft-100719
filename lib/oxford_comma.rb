4315a4618e6799ec7aa999e7afb84becb3070c01
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 16:51:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,11 @@ def oxford_comma(array)
   if array.length == 1 
     array.join
   else
+    new_word = ""
     array.each_with_index do |part, index|
-      
+      if index + 1 == array.length
+        
+      end
     end
   end
 end
